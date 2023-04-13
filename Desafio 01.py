0549de3a3c197d6722cd79def97a37ab7fd9d008
Rodrigo Gama <121040163+FRgama@users.noreply.github.com>
2023-04-13 20:43:12 -0300

    Update Desafio 01.py

--- Desafio 01.py
+++ Desafio 01.py
@@ -1,3 +1,6 @@
+#Escreva um programa que calcule a média de três números digitados pelo usuário. O programa deve pedir que o usuário digite os três números, calcular a média e exibir o resultado na tela.
+#Dica: Use a função input() para solicitar a entrada do usuário e a função print() para exibir o resultado.
+
 escolha = float(input('escollha um numero: '))
 escolha2 = float(input('escollha outro numero: '))
 escolha3 = float(input('escollha mais um numero: '))
