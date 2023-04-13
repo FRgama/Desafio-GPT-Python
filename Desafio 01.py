7cb659935a641c94a4af12abce8d13338c025a5d
Rodrigo Gama <121040163+FRgama@users.noreply.github.com>
2023-04-13 20:40:57 -0300

    Add files via upload

---
+++ Desafio 01.py
@@ -0,0 +1,6 @@
+escolha = float(input('escollha um numero: '))
+escolha2 = float(input('escollha outro numero: '))
+escolha3 = float(input('escollha mais um numero: '))
+media = ((escolha + escolha2 + escolha3)/3)
+print ('A média dos números {}, {} e {} escolhidos foi {:.2f}' .format(escolha, escolha2, escolha3, media))
+
